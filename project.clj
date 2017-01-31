e396c0452df129df505607f8fad06c8a331de6ad
OHTA Shogo <athos0220@gmail.com>
2017-01-31 18:53:20 +0900

    Integrant'ize whole system

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
                  [org.clojure/core.async "0.2.391"
-                  :exclusions [org.clojure/tools.reader]]]
+                  :exclusions [org.clojure/tools.reader]]
+                 [integrant "0.2.0"]]
 
   :plugins [[lein-figwheel "0.5.9-SNAPSHOT"]
             [lein-cljsbuild "1.1.5" :exclusions [[org.clojure/clojure]]]]
--- src/bitbitbot/core.cljs
+++ src/bitbitbot/core.cljs
@@ -1,50 +1,63 @@
 (ns bitbitbot.core
-  (:require [cljs.nodejs :as nodejs]))
+  (:require [cljs.nodejs :as nodejs]
+            [integrant.core :as ig]))
 
 (nodejs/enable-util-print!)
 
 (defonce builder (nodejs/require "botbuilder"))
 (defonce restify (nodejs/require "restify"))
 
-(defonce connector
-  (let [id (.. js/process -env -MICROSOFT_APP_ID)
-        password (.. js/process -env -MICROSOFT_APP_PASSWORD)
-        settings #js{:appId id :appPassword password}]
-    (builder.ChatConnector. settings)))
+;; necessary to guarantee by ourselves system has a singleton connector
+;; in spite of our use of Integrant
 
-(defonce +server+ (atom nil))
+(defonce connector (atom nil))
 
-(defn start-server []
+(defmethod ig/init-key :connector [_ {:keys [id password]}]
+  (or @connector
+      (let [settings #js{:appId id :appPassword password}
+            conn (builder.ChatConnector. settings)]
+        (reset! connector conn)
+        conn)))
+
+(defmethod ig/init-key :server [_ {:keys [port connector]}]
   (js/console.log "starting server ...")
-  (let [port (or (.. js/process -env -PORT) 3978)
-        server (doto (.createServer restify)
-                 (.post "/api/messages" (.listen connector))
-                 (.listen port #(js/console.log "server started.")))]
-    (reset! +server+ server)))
+  (doto (.createServer restify)
+    (.post "/api/messages" (.listen connector))
+    (.listen port #(js/console.log "server started."))))
 
-(defn stop-server []
+(defmethod ig/halt-key! :server [_ server]
   (js/console.log "stopping server ...")
-  (.close @+server+)
-  (reset! +server+ nil))
+  (.close server))
 
-(defonce +bot+ (atom nil))
-
-(defn start-bot []
-  (let [bot (builder.UniversalBot. connector)]
-    (.dialog bot "/"
+(defmethod ig/init-key :bot [_ {:keys [connector]}]
+  (doto (builder.UniversalBot. connector)
+    (.dialog "/"
       (fn [session]
-        (.send session "Hello World")))
-    (reset! +bot+ bot)))
+        (.send session "Hello World")))))
 
-(defn setup []
-  (start-server)
-  (start-bot))
+(def config
+  {:connector {:id (.. js/process -env -MICROSOFT_APP_ID)
+               :password (.. js/process -env -MICROSOFT_APP_PASSWORD)}
+   :server    {:port (or (.. js/process -env -PORT) 3978)
+               :connector (ig/ref :connector)}
+   :bot       {:connector (ig/ref :connector)}})
+
+(defonce system (atom nil))
+
+(defn start []
+  (when-not @system
+    (reset! system (ig/init config))))
+
+(defn stop []
+  (when-let [s @system]
+    (ig/halt! s)
+    (reset! system nil)))
 
 (defn -main []
-  (setup))
+  (start))
 
 (defn reload []
-  (stop-server)
-  (setup))
+  (stop)
+  (start))
 
 (set! *main-cli-fn* -main)
