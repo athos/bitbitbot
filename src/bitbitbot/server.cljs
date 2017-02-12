99ed0064c9f1b088cb486eaa91080ac889139c41
OHTA Shogo <athos0220@gmail.com>
2017-02-12 16:35:29 +0900

    Output reloading messages to stderr

--- src/bitbitbot/server.cljs
+++ src/bitbitbot/server.cljs
@@ -6,11 +6,11 @@
 (defn start-server [port connector]
   (let [port (or (some-> (not-empty port) js/parseInt)
                  3978)]
-    (js/console.log "starting server ...")
+    (.. js/process -stderr (write "starting server ...\n"))
     (doto (restify.createServer)
       (.post "/api/messages" (.listen connector))
-      (.listen port #(js/console.log "server started.")))))
+      (.listen port #(.. js/process -stderr (write "server started.\n"))))))
 
 (defn stop-server [server]
-  (js/console.log "stopping server ...")
+  (.. js/process -stderr (write "stopping server ...\n"))
   (.close server))
